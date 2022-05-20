3bcb804a50de7d837d9f6fd8d5f5a6835cc61f88
HARRISH <96364633+mharrish7@users.noreply.github.com>
2022-05-20 08:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, jsonify,render_template,request,redirect
+
+from flask import Flask, g, jsonify,render_template,request,redirect,session
+import sqlite3
+
+
+
+
+
+
+def get_U():
+    con = sqlite3.connect('US.db')
+    cursor = con.cursor()
+    cursor.execute('select * from USPW1')
+    Users = []
+    for i in cursor:
+        Users.append(i)
+    return Users
 
 app = Flask(__name__)
+app.secret_key = "harrish"
+#session['user'] = 'nologin'
 
-Users = [['h1@gmail.com','h1'],['h2@gmail.com','h2']]
+Users = get_U()
 
 @app.route('/')
 def home():
+    session['user'] = 'nologin'
     return render_template('index.html')
 
 @app.route('/register')
@@ -16,13 +35,34 @@ def reg():
 def log():
     em = request.form['et']
     pw = request.form['pt']
-    if [em,pw] in Users:
+    Users = get_U()
+    if (em,pw) in Users:
+        session['user'] = em
         return jsonify({'login': True})
     else:
         return jsonify({'login': False})
 
+
+@app.route('/reg',methods = ['POST'])
+def regs():
+    try:
+        con = sqlite3.connect('US.db')
+        cursor = con.cursor()
+        em = request.form['ret']
+        pw = request.form['rpt']
+        pcw = request.form['rcpt']
+        if pcw == pw:
+            cursor.execute(f'insert into USPW1 values("{em}","{pw}")')
+            con.commit()
+            
+            return jsonify({'data':True,'info':'registered'})
+        else:
+            return jsonify({'data':False,'info':'passwords not matching'})
+    except:
+         return jsonify({'data':False,'info':'error or Username is taken'})
+
 @app.route('/home')
 def ho():
-    return render_template('home.html')
+    return render_template('home.html',name = session['user'])
 
 app.run(debug=True)
